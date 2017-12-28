3057a2b43c5a264785d97f37db8a286cdcc995bf
sonichy <sonichy@163.com>
2017-12-28 11:16:27 +0800

    add: sound, soft, refresh

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,13 +11,21 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     QStringList SL;
-    SL << "总览" << "系统" << "处理器" << "主板" << "内存" << "硬盘" << "显卡" << "显示器" << "其他";
+    SL << "总览" << "系统" << "处理器" << "主板" << "内存" << "硬盘" << "显卡" << "显示器" << "声卡" << "软件";
     ui->listWidget->addItems(SL);
     connect(ui->listWidget,SIGNAL(currentRowChanged(int)),this,SLOT(currentRowChange(int)));
-    move((QApplication::desktop()->width()-width())/2, (QApplication::desktop()->height()-height())/2);
-    ui->label->setText("获取硬件信息...");
+    move((QApplication::desktop()->width()-width())/2, (QApplication::desktop()->height()-height())/2);    
 
-    // CPU
+    refresh();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::getCPUInfo()
+{
     QFile file("/proc/cpuinfo");
     file.open(QIODevice::ReadOnly);
     QString content = file.readAll();
@@ -27,13 +35,15 @@ MainWindow::MainWindow(QWidget *parent) :
     cpu_model = modelline.first().split(":").at(1).split("@").first();
     QStringList coreline = lines.filter("cpu cores");
     cpu_core = coreline.first().split(":").at(1);
+}
 
-    // GPU
+void MainWindow::getGPUInfo()
+{
     QProcess *process = new QProcess;
     process->start("lshw -C display");
     process->waitForFinished();
     QString PO = process->readAllStandardOutput();
-    SL = PO.split("\n");
+    QStringList SL = PO.split("\n");
     for(int i=0; i<SL.size(); i++){
         if (SL.at(i).contains("product:")) {
             gpu_model = SL.at(i);
@@ -63,13 +73,52 @@ MainWindow::MainWindow(QWidget *parent) :
             }
         }
     }
-
-    ui->listWidget->setCurrentRow(0);
 }
 
-MainWindow::~MainWindow()
+void MainWindow::getSoundInfo()
 {
-    delete ui;
+    QProcess *process = new QProcess;
+    process->start("lshw -C multimedia");
+    process->waitForFinished();
+    QString PO = process->readAllStandardOutput();
+    QStringList SL = PO.split("\n");
+    for(int i=0; i<SL.size(); i++){
+        if (SL.at(i).contains("product:")) {
+            sound_model = SL.at(i);
+            sound_model.remove("product:");
+        }
+        if(SL.at(i).contains("vendor:")) {
+            sound_vendor = SL.at(i);
+            sound_vendor.remove("vendor:");
+            sound_vendor.remove("Corporation");
+        }
+        if(SL.at(i).contains("width:")) {
+            sound_width = SL.at(i);
+            sound_width.remove("width:");
+        }
+        if(SL.at(i).contains("clock:")) {
+            sound_clock = SL.at(i);
+            sound_clock.remove("clock:");
+        }
+        if(SL.at(i).contains("driver=")) {
+            QStringList SL1 = SL.at(i).split(QRegExp("\\s{1,}")); // 第一个\表示转义字符，\s表示空格，｛1，｝表示一个以上
+            //qDebug() << SL1;
+            for(int j=0; j<SL1.size(); j++){
+                if(SL1.at(j).contains("driver=")){
+                    sound_driver = SL1.at(j);
+                    sound_driver.remove("driver=");
+                }
+            }
+        }
+    }
+}
+
+void MainWindow::getSoftware()
+{
+    QProcess *process = new QProcess;
+    process->start("dpkg -l");
+    process->waitForFinished();
+    software = process->readAllStandardOutput();
 }
 
 void MainWindow::currentRowChange(int row) //itemClick(QListWidgetItem *item)
@@ -77,33 +126,49 @@ void MainWindow::currentRowChange(int row) //itemClick(QListWidgetItem *item)
     //Q_UNUSED(item);
     switch (row) {
     case 0:
-        ui->label->setText("系统：" + QSysInfo::prettyProductName() + "\n处理器：" + cpu_model + "\n显卡：" + gpu_vendor + gpu_model);
+        ui->textBrowser->setText("系统：" + QSysInfo::prettyProductName() + "\n处理器：" + cpu_model + "\n显卡：" + gpu_vendor + gpu_model + "\n声卡：" + sound_vendor + sound_model);
         break;
     case 1:
-        ui->label->setText("用户名：" + qgetenv("USER") + "\n核心类型：" + QSysInfo::kernelType() + "\n核心版本：" + QSysInfo::kernelVersion() + "\n发行版：" + QSysInfo::prettyProductName() + "\n发行版本号：" + QSysInfo::productVersion());
+        ui->textBrowser->setText("用户名：" + qgetenv("USER") + "\n核心类型：" + QSysInfo::kernelType() + "\n核心版本：" + QSysInfo::kernelVersion() + "\n发行版：" + QSysInfo::prettyProductName() + "\n发行版本号：" + QSysInfo::productVersion());
         break;
     case 2:
-        ui->label->setText("型号：" + cpu_model + "\n核心数：" + cpu_core + "\n架构：" + QSysInfo::currentCpuArchitecture());
+        ui->textBrowser->setText("型号：" + cpu_model + "\n核心数：" + cpu_core + "\n架构：" + QSysInfo::currentCpuArchitecture());
         break;
     case 3:
-        ui->label->setText("");
+        ui->textBrowser->setText("");
         break;
     case 4:
-        ui->label->setText("");
+        ui->textBrowser->setText("");
         break;
     case 5:
-        ui->label->setText("");
+        ui->textBrowser->setText("");
         break;
     case 6:
-        ui->label->setText("型号：" + gpu_model + "\n厂商：" + gpu_vendor + "\n带宽：" + gpu_width + "\n频率：" + gpu_clock + "\n驱动：\t" + gpu_driver);
+        ui->textBrowser->setText("型号：" + gpu_model + "\n厂商：" + gpu_vendor + "\n带宽：" + gpu_width + "\n频率：" + gpu_clock + "\n驱动：\t" + gpu_driver);
         break;
     case 7:
-        ui->label->setText("");
+        ui->textBrowser->setText("");
         break;
     case 8:
-        ui->label->setText("");
+        ui->textBrowser->setText("型号：" + sound_model + "\n厂商：" + sound_vendor + "\n带宽：" + sound_width + "\n频率：" + sound_clock + "\n驱动：\t" + sound_driver);
         break;
-    default:
+    case 9:
+        ui->textBrowser->setText(software);
         break;
     }
+}
+
+void MainWindow::refresh()
+{
+    ui->textBrowser->setText("正在获取硬件信息...");
+    getCPUInfo();
+    getGPUInfo();
+    getSoundInfo();
+    getSoftware();
+    ui->listWidget->setCurrentRow(0);
+}
+
+void MainWindow::on_actionRefresh_triggered()
+{
+    refresh();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,10 +18,16 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QString cpu_model, cpu_core, gpu_model, gpu_vendor, gpu_width, gpu_clock, gpu_driver;
+    QString cpu_model, cpu_core, gpu_model, gpu_vendor, gpu_width, gpu_clock, gpu_driver, sound_model, sound_vendor, sound_width, sound_clock, sound_driver, software;
+    void getCPUInfo();
+    void getGPUInfo();
+    void getSoundInfo();
+    void getSoftware();
+    void refresh();
 
 private slots:
     void currentRowChange(int row);
+    void on_actionRefresh_triggered();
 
 };
 
