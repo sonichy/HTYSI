1c7f576b52c716f4ab1e0fb0bc7da192c6348b3b
sonichy <sonichy@163.com>
2018-12-27 18:30:15 +0800

    add AMD brand image

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,12 +10,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    move((QApplication::desktop()->width() - width())/2, (QApplication::desktop()->height() - height())/2);
     QStringList SL;
-    SL << "总览" << "系统" << "处理器" << "主板" << "内存" << "硬盘" << "显卡" << "显示器" << "声卡" << "软件";
+    SL << "总览" << "系统" << "处理器" << "主板" << "内存" << "硬盘" << "显卡" << "显示器" << "声卡" << "网卡" << "软件";
     ui->listWidget->addItems(SL);
     connect(ui->listWidget,SIGNAL(currentRowChanged(int)),this,SLOT(currentRowChange(int)));
-    move((QApplication::desktop()->width()-width())/2, (QApplication::desktop()->height()-height())/2);    
-
     refresh();
 }
 
@@ -32,9 +31,15 @@ void MainWindow::getCPUInfo()
     file.close();
     QStringList lines = content.split("\n");
     QStringList modelline = lines.filter("model name");
-    cpu_model = modelline.first().split(":").at(1).split("@").first();
+    cpu_model = modelline.first().split(":").at(1);
     QStringList coreline = lines.filter("cpu cores");
     cpu_core = coreline.first().split(":").at(1);
+    QStringList cacheline = lines.filter("cache size");
+    cpu_cache = cacheline.first().split(":").at(1);
+    QStringList bugsline = lines.filter("bugs");
+    cpu_bugs = bugsline.first().split(":").at(1);
+    QStringList flagsline = lines.filter("flags");
+    cpu_flags = flagsline.first().split(":").at(1);
 }
 
 void MainWindow::getGPUInfo()
@@ -90,7 +95,6 @@ void MainWindow::getSoundInfo()
         if(SL.at(i).contains("vendor:")) {
             sound_vendor = SL.at(i);
             sound_vendor.remove("vendor:");
-            sound_vendor.remove("Corporation");
         }
         if(SL.at(i).contains("width:")) {
             sound_width = SL.at(i);
@@ -113,6 +117,33 @@ void MainWindow::getSoundInfo()
     }
 }
 
+void MainWindow::getNetwork()
+{
+    QProcess *process = new QProcess;
+    process->start("lshw -C network");
+    process->waitForFinished();
+    QString PO = process->readAllStandardOutput();
+    QStringList SL = PO.split("\n");
+    for(int i=0; i<SL.size(); i++){
+        if (SL.at(i).contains("product:")) {
+            npu_model = SL.at(i);
+            npu_model.remove("product:");
+        }else if (SL.at(i).contains("vendor:")) {
+            npu_vendor = SL.at(i);
+            npu_vendor.remove("vendor:");
+        }else if (SL.at(i).contains("capacity:")) {
+            npu_capacity = SL.at(i);
+            npu_capacity.remove("capacity:");
+        }else if(SL.at(i).contains("width:")) {
+            npu_width = SL.at(i);
+            npu_width.remove("width:");
+        }else if(SL.at(i).contains("clock:")) {
+            npu_clock = SL.at(i);
+            npu_clock.remove("clock:");
+        }
+    }
+}
+
 void MainWindow::getSoftware()
 {
     QProcess *process = new QProcess;
@@ -121,18 +152,28 @@ void MainWindow::getSoftware()
     software = process->readAllStandardOutput();
 }
 
-void MainWindow::currentRowChange(int row) //itemClick(QListWidgetItem *item)
+void MainWindow::currentRowChange(int row)
 {
-    //Q_UNUSED(item);
+    ui->label_brand->setPixmap(QPixmap(""));
     switch (row) {
     case 0:
         ui->textBrowser->setText("系统：" + QSysInfo::prettyProductName() + "\n处理器：" + cpu_model + "\n显卡：" + gpu_vendor + gpu_model + "\n声卡：" + sound_vendor + sound_model);
         break;
     case 1:
         ui->textBrowser->setText("用户名：" + qgetenv("USER") + "\n核心类型：" + QSysInfo::kernelType() + "\n核心版本：" + QSysInfo::kernelVersion() + "\n发行版：" + QSysInfo::prettyProductName() + "\n发行版本号：" + QSysInfo::productVersion());
+        if(QSysInfo::prettyProductName().contains("Deepin")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/deepin.jpg").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }
         break;
     case 2:
-        ui->textBrowser->setText("型号：" + cpu_model + "\n核心数：" + cpu_core + "\n架构：" + QSysInfo::currentCpuArchitecture());
+        ui->textBrowser->setText("型号：" + cpu_model + "\n核心数：" + cpu_core + "\n缓存：" + cpu_cache + "\n架构：" + QSysInfo::currentCpuArchitecture() + "\n特征：" + cpu_flags + "\n缺陷：" + cpu_bugs);
+        if(cpu_model.contains("Celeron")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/celeron.png").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }else if(cpu_model.contains("Pentium")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/pentium.png").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }else if(cpu_model.contains("AMD")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/AMD.png").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }
         break;
     case 3:
         ui->textBrowser->setText("");
@@ -145,6 +186,13 @@ void MainWindow::currentRowChange(int row) //itemClick(QListWidgetItem *item)
         break;
     case 6:
         ui->textBrowser->setText("型号：" + gpu_model + "\n厂商：" + gpu_vendor + "\n带宽：" + gpu_width + "\n频率：" + gpu_clock + "\n驱动：\t" + gpu_driver);
+        if(gpu_vendor.contains("Intel")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/intel.png").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }else if(gpu_vendor.contains("Nvidia")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/NVIDIA.svg").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }else if(gpu_vendor.contains("Radeon") || gpu_model.contains("Radeon")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/radeon.jpg").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }
         break;
     case 7:
         ui->textBrowser->setText("");
@@ -153,6 +201,12 @@ void MainWindow::currentRowChange(int row) //itemClick(QListWidgetItem *item)
         ui->textBrowser->setText("型号：" + sound_model + "\n厂商：" + sound_vendor + "\n带宽：" + sound_width + "\n频率：" + sound_clock + "\n驱动：\t" + sound_driver);
         break;
     case 9:
+        ui->textBrowser->setText("型号：" + npu_model + "\n厂商：" + npu_vendor + "\n带宽：" + npu_width + "\n频率：" + npu_clock + "\n速率：" + npu_capacity);
+        if(npu_vendor.contains("Realtek")){
+            ui->label_brand->setPixmap(QPixmap(":/icon/realtek.gif").scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+        }
+        break;
+    case 10:
         ui->textBrowser->setText(software);
         break;
     }
@@ -164,6 +218,7 @@ void MainWindow::refresh()
     getCPUInfo();
     getGPUInfo();
     getSoundInfo();
+    getNetwork();
     getSoftware();
     ui->listWidget->setCurrentRow(0);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,10 +18,11 @@ public:
 
 private:
     Ui::MainWindow *ui;
-    QString cpu_model, cpu_core, gpu_model, gpu_vendor, gpu_width, gpu_clock, gpu_driver, sound_model, sound_vendor, sound_width, sound_clock, sound_driver, software;
+    QString cpu_model, cpu_core, cpu_cache, cpu_bugs, cpu_flags, gpu_model, gpu_vendor, gpu_width, gpu_clock, gpu_driver, sound_model, sound_vendor, sound_width, sound_clock, sound_driver, npu_model, npu_vendor, npu_capacity, npu_width, npu_clock, software;
     void getCPUInfo();
     void getGPUInfo();
     void getSoundInfo();
+    void getNetwork();
     void getSoftware();
     void refresh();
 
